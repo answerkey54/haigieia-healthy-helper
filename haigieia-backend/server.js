ac560dcd0f8b0a4157c9ff9d2af47c0f6102fd31
MaxwellLi-Umich <maxwelli@umich.edu>
2022-12-07 14:11:18 -0500

    buttoned up the expressjs server

--- haigieia-backend/server.js
+++ haigieia-backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios').default;
+const fetch = require('node-fetch')
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
@@ -7,20 +9,36 @@ app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.raw({extended: true}))
 
 const port = 3000
+const token = "" // make sure to set the key after you instantiate the jaseci server!!
 
 app.get('/', (req, res) => {
   res.send('Haigeia Backend Running')
 })
 
-app.post('/audio', (req, res) => {
+app.post('/audio', (req, res) => 
   var path = req.body["path"]
   var spawn = require("child_process").spawn;
   var python = spawn('python', ["./src/speech-to-text/stt.py", path, "base"]);
   console.log(path)
   console.log("spawned child process to generate text for ", path)
-
   python.stdout.on('data', function(data) {
-    console.log("whisper: ", data.toString())
+    console.log("whisper: ", data.toString())    
+    var message =  data.toString
+    const body  = {
+      "ctx": {
+          "question": `${message}`,
+      },
+      "name": "talk"
+    }
+    const repsonse = await fetch("http://localhost/8000/walker-run",{
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        'Authorization': `token ${token}`
+        }
+    });
+    const data = await response.json();
     res.send(data.toString())
   });
 
@@ -32,8 +50,29 @@ app.post('/audio', (req, res) => {
   python.on('exit', function(code){
     console.log(`STT child process exited with code ${code}`) 
   })
-  
 });
+
+app.post('/text', (req, res) => 
+  // make sure to correctly formulate the body and the endpoint
+  var message = req.body["message"]
+  const body  = {
+    "ctx": {
+        "question": `${message}`,
+    },
+    "name": "talk"
+  }
+  const repsonse = await fetch("http://localhost/8000/walker-run",{
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      'Authorization': `token ${token}`
+      }
+  });
+  const data = await response.json();
+  res.send(data.toString())
+});
+
 
 app.listen(port, () => {
   console.log(`Haigeia Backend listening on port ${port}`)
